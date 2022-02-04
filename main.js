a188b50c5b6d23cc691291177b1d97260388966e
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-02-04 14:57:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+
+var canvas = new fabric.Canvas('nyCemuez')
+
+by=0
+bx=0
+hy=400
+hx=800
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	fabric.Image.fromURL("golf-h1.png", function (Img) {
+		hj = Img;
+		hj.scaleToWidth(50)
+		hj.scaleToHeight(50)
+		hj.set({
+			top:hy,
+			left:hx
+		})
+		canvas.add(hole_obj)
+	})
+	new_image();
+}
+
+function new_image()
+{
+	fabric.Image.fromURL("ball.png", function(Img) {
+		bj = Img;
+		bj.scaleToWidth(50)
+		bj.scaleToHeight(50)
+		bj.set({
+			top:by,
+			left:bx
+		})
+		canvas.add(ball_obj)
+	})
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if ((bx==hx)&&(by==hy)) {
+		canvas.remove(bj)
+		document.getElementById("hd3").innerHTML="Bottom text"
+		document.getElementById("nyCemuez").style.borderColor="red"
+	}
+	
+	else {
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		if (by > 5) {
+			by += block_image_height
+			canvas.remove(ball_obj)
+			new_image()
+		}
+	}
+
+	function down()
+	{
+		if (by <= 450) {
+			by += block_image_height
+			canvas.remove(ball_obj)
+			new_image()
+		}
+	}
+
+	function left()
+	{
+		if(bx>5)
+		{
+			bx -= block_image_width
+			canvas.remove(ball_obj)
+			new_image()
+		}
+	}
+
+	function right()
+	{
+		if(bx <=1050)
+		{
+			bx -= block_image_width
+			canvas.remove(ball_obj)
+			new_image()
+		}
+	}
+	
+}
+
